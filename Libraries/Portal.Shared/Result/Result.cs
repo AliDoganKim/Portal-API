c5687e2ef2c299d5d9fe1710759849ddb9706686
Ali DOĞAN <alidogans@outlook.com>
2020-11-26 15:35:06 +0300

    ders 10 : Response'ların tutarlılığı

---
+++ Libraries/Portal.Shared/Result/Result.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Portal.Shared.Result
+{
+    public class Result
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+        public Result(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public static Result<T> Ok<T>(IQueryable<T> value, string message)
+        {
+            return new Result<T>(value, true, message);
+        }
+
+        public static ResultSingle<T> Ok<T>(T value, string message)
+        {
+            return new ResultSingle<T>(value, true, message);
+        }
+
+        public static Result<T> Fail<T>(string message)
+        {
+            return new Result<T>(null, false, message);
+        }
+    }
+}
---
+++ Libraries/Portal.Shared/Result/ResultSingle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Shared.Result
+{
+    public class ResultSingle<T> : Result
+    {
+        public T Value { get; set; }
+
+        public ResultSingle(T value, bool success, string message) : base(success, message)
+        {
+            Value = value;
+        }
+    }
+}
